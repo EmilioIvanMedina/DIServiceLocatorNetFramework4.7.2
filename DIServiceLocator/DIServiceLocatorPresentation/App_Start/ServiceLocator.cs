de534ab595034b4e8e360cf53c9036638021a5b7
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-08-21 20:48:33 -0300

    adding a ServiceLocator class, to resolve T (any type) request of injection

---
+++ DIServiceLocator/DIServiceLocatorPresentation/App_Start/ServiceLocator.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DIServiceLocatorPresentation.App_Start
+{
+    public static class ServiceLocator
+    {
+        public static T GetService<T>()
+        {
+            return (T)DependencyResolver.Current.GetService(typeof(T));
+        }
+    }
+}
