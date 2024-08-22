5d77619721955952c9867bbf00e157371ef21149
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-08-22 20:45:45 -0300

    configuring the golbal.asax with the IcC container

--- DIServiceLocator/DIServiceLocatorPresentation/Global.asax.cs
+++ DIServiceLocator/DIServiceLocatorPresentation/Global.asax.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using DIServiceLocatorPresentation.App_Start;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -10,12 +9,20 @@ namespace DIServiceLocatorPresentation
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static IServiceProvider ServiceProvider { get; private set; }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var serviceCollection = new ServiceCollection();
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            DependencyResolver.SetResolver(new ServiceProviderDependencyResolver(ServiceProvider));
         }
     }
 }
