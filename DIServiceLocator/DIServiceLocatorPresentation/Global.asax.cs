346621583e3650242935ad7d2327a149964e6a62
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-08-22 21:04:45 -0300

    Registering the service in the IoC (global.asax) - injecting into controller

--- DIServiceLocator/DIServiceLocatorPresentation/Global.asax.cs
+++ DIServiceLocator/DIServiceLocatorPresentation/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using DIServiceLocatorPresentation.App_Start;
+﻿using DIServiceLocatorInfrastructure.Implementations;
+using DIServiceLocatorInfrastructure.Interfaces;
+using DIServiceLocatorPresentation.App_Start;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Web.Mvc;
@@ -20,9 +22,16 @@ namespace DIServiceLocatorPresentation
 
             var serviceCollection = new ServiceCollection();
 
+            ConfigureServices(serviceCollection);
+
             ServiceProvider = serviceCollection.BuildServiceProvider();
 
             DependencyResolver.SetResolver(new ServiceProviderDependencyResolver(ServiceProvider));
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<ISampleService, SampleService>();
         }
     }
 }
